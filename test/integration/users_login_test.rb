71a89f87ab56596ca1db0b1411bc80b33f2e8000
karirin <karirin419@gmail.com>
2020-03-19 07:25:49 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "login with invalid information" do
